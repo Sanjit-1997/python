923f924a582015a94a7ee8286835be530274e45e
Sanjit-1997 <129972022+Sanjit-1997@users.noreply.github.com>
2023-09-13 20:42:01 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,12 @@
+class area:
+    def sanjit(self,x,y):
+        self.new=x
+        self.new1=y
+    def show(self):
+        print(self.new*self.new1
+              )
+
+        
+obj=area()
+obj.sanjit(5,6)
+obj.show()
